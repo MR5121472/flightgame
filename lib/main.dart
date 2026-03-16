3adb5b85c3aee01e088e7a9c9719daf93c89be86
MR5121472 <royalfaizan61@gmail.com>
2026-03-16 18:58:45 +0500

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+import 'ui/game_screen.dart';
 
+void main() {
+  runApp(const FlightApp());
+}
+
+class FlightApp extends StatelessWidget {
+  const FlightApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: GameScreen(),
+    );
+  }
+}
