939007e7d8efee1b18b26a2104a36fd64a325d57
MR5121472 <royalfaizan61@gmail.com>
2026-03-16 18:57:44 +0500

    Add files via upload

--- lib/ui/game_screen.dart
+++ lib/ui/game_screen.dart
@@ -1,1 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flame/game.dart';
+import '../game/flight_game.dart';
 
+class GameScreen extends StatefulWidget {
+  const GameScreen({super.key});
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+
+  late FlightGame game;
+
+  @override
+  void initState() {
+    super.initState();
+    game = FlightGame();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+
+          GameWidget(game: game),
+
+          Positioned(
+            bottom: 120,
+            left: 20,
+            child: ElevatedButton(
+              onPressed: () {
+                game.thrust();
+              },
+              child: const Text("THRUST"),
+            ),
+          ),
+
+          Positioned(
+            bottom: 120,
+            right: 20,
+            child: ElevatedButton(
+              onPressed: () {
+                game.brake();
+              },
+              child: const Text("BRAKE"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
